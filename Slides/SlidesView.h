0939ffd3339bc8e5f8709ba73ed5d7f0a03554b3
oles <svider.dev@gmail.com>
2020-01-06 11:20:10 +0200

    Initial Commit

---
+++ Slides/SlidesView.h
@@ -0,0 +1,13 @@
+//
+//  SlidesView.h
+//  Slides
+//
+//  Created by oles on 1/6/20.
+//  Copyright © 2020 oles. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SlidesView : ScreenSaverView
+
+@end
---
+++ Slides/SlidesView.m
@@ -0,0 +1,52 @@
+//
+//  SlidesView.m
+//  Slides
+//
+//  Created by oles on 1/6/20.
+//  Copyright © 2020 oles. All rights reserved.
+//
+
+#import "SlidesView.h"
+
+@implementation SlidesView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
