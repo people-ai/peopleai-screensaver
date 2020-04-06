8f8753a10ca24830e2e6c155e4cfd6947d2c42ca
oles <svider.dev@gmail.com>
2020-04-06 23:09:35 +0300

    scripts updated

--- notarize.sh
+++ notarize.sh
@@ -1,2 +1,2 @@
 # Notarization
-xcrun altool --notarize-app --primary-bundle-id "ai.people.screensaver" --username "svider.dev@gmail.com" --password "mlpo-nwfh-wslo-ctwh" --asc-provider "B865KYJU5B" --file "Build/People.ai.signed.zip"
+xcrun altool --notarize-app --primary-bundle-id "ai.people.screensaver" --username "[APPLE ID]" --password "[APP PASSWORD]" --asc-provider "B865KYJU5B" --file "Build/People.ai.signed.zip"
