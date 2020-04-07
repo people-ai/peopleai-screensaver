855744340ef62726bc02a74f58815b43294aabc4
oles <svider.dev@gmail.com>
2020-04-07 21:49:34 +0300

    [*] Removed some commented code

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # build
 xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" clean
 xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" build
-xcodebuild -project People.ai.xcodeproj -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" archive -archivePath "Build/People.ai"
-xcodebuild -project People.ai.xcodeproj -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" archive -exportArchive -exportOptionsPlist "exportOptions.plist"
+xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" archive -archivePath "Build/People.ai.xcarchive"
+xcodebuild -project People.ai.xcodeproj -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" -exportArchive -archivePath "Build/People.ai.xcarchive" -exportOptionsPlist "exportOptions.plist"
 
 # package
 packagesbuild -v "People.ai.pkgproj"
