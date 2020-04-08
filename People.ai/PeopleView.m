4a74a64fbeded4ab5e4c6745444296288f94b008
George Kozlov <george@people.ai>
2020-04-07 20:18:29 -0700

    Removed legacy and experimental code.

--- People.ai/PeopleView.m
+++ People.ai/PeopleView.m
@@ -15,7 +15,6 @@ static CGFloat resizeWidth = 0.05; // resize
 static CGFloat resizeHeight = 0.05; // resize
 
 static NSString *currentLink = @"";
-//static int previousSlide = 0;
 static NSTimer *timer;
 
 @implementation PeopleView
@@ -23,8 +22,6 @@ static NSTimer *timer;
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview {
     self = [super initWithFrame:frame isPreview:isPreview];
     if (self) {
-        //[self setAnimationTimeInterval:1/30.0];
-        
         self.webView = [[WKWebViewCustom alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
         [self addSubview:self.webView];
         self.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
@@ -46,11 +43,7 @@ static NSTimer *timer;
                 [self checkViewRefreshTime];
                 [self setAnimationTimeInterval:1];
             });
-        } else {
-            //[self loadConfiguartionFromBundle];
-            //[self reloadPage];
         }
-    
     }
     return self;
 }
@@ -89,12 +82,10 @@ static NSTimer *timer;
 
 - (void)animateOneFrame {
     if (mdmMode) {
-        // do nothing, just save current slide
         [self saveCurrentSlide];
     } else {
         if (self.currentSlide < self.maxSlides) {
             self.currentSlide ++;
-            //[self reloadPage];
         } else {
             [self loadInfoMessage:noMoreSlidesError];
         }
@@ -176,13 +167,8 @@ static NSTimer *timer;
             [self.webView setFrame:NSMakeRect(-(resizeWidth*self.bounds.size.width), -(resizeHeight*self.bounds.size.height), self.bounds.size.width*(1+2*resizeWidth), self.bounds.size.height*(1 + 2 * resizeHeight))];
         }
     } else {
-        /*
-        NSString *msg = [NSString stringWithFormat:@"Error: link: %@, time: %@, moduleName: %@", link, stayOnSlideTime, moduleName];
-        [self loadInfoMessage:msg];
-        */
         [self loadErrorPage];
     }
-    
     
     if (debugMode) {
         [self showDebugMessage:[NSString stringWithFormat:@"loadConfig parent view rect: %@", NSStringFromRect(self.frame)]];
@@ -190,38 +176,6 @@ static NSTimer *timer;
     }
 
 }
-
-/*
-- (void)loadConfiguartionFromBundle {
-    NSDictionary *mainConfiguration = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle bundleWithIdentifier:@"com.test.Slides"] pathForResource:configFile ofType:@"plist" inDirectory:@""]];
-
-    NSString *link = mainConfiguration[urlKey];
-    NSNumber *resetSlidesWhenStarted = mainConfiguration[resetKey];
-    NSNumber *stayOnSlideTime = mainConfiguration[timeKey];
-    NSNumber *max = mainConfiguration[maxSlidesKey];
-    
-    if (resetSlidesWhenStarted && resetSlidesWhenStarted.boolValue) {
-        self.currentSlide = 0;
-    } else {
-        NSNumber *slide = [[NSUserDefaults standardUserDefaults] objectForKey:currentSlideKey];
-        if (slide) {
-            self.currentSlide = slide.intValue;
-        } else {
-            self.currentSlide = 0;
-        }
-    }
-    self.maxSlides = 3;
-    if (link) {
-        self.baseLink = link;
-        self.slideTime = stayOnSlideTime.intValue;
-        self.maxSlides = max.intValue;
-    } else {
-        // show some default image or text
-        [self loadInfoMessage:configError];
-    }
-    [self setAnimationTimeInterval:self.slideTime]; // from ms to sec
-}
-*/
 
 - (NSString *)create:(NSString *)link Mode:(NSString *)mode slide:(int)slide {
     return [NSString stringWithFormat:@"%@/preview?rm=%@&slide=%i", link, mode, slide];
@@ -234,20 +188,6 @@ static NSTimer *timer;
         return [NSString stringWithFormat:@"%@?rm=minimal&start=true&loop=true&delayms=%d", link, time*1000];
     }
 }
-
-/*
-- (void)reloadPage {
-    if (self.baseLink) {
-        NSString *link = [self create:self.baseLink Mode:modeMinimal slide:self.currentSlide];
-        if (link) {
-            NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:link]];
-            [self.webView loadRequest:request];
-        }
-        [[NSUserDefaults standardUserDefaults] setObject:@(self.currentSlide) forKey:currentSlideKey];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
-}
-*/
 
 - (void)loadInfoMessage:(NSString *)msg {
     [self.webView loadHTMLString:msg baseURL:nil];
