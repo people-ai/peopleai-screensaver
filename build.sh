2e5a239c4792cb4451729790bdeb05af5c6f445c
George Kozlov <george@people.ai>
2020-04-08 14:34:32 -0700

    Updated build scripts and configuration for better compatibility. We define build path now and do not depend on buggy archive export.

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
-#
-#  Created by People.ai on 1/21/20.
-#  Copyright © 2020 People.ai, Inc. All rights reserved.
-#
-
 # build
 xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" clean
-xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" build
+xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" build -xcconfig build.xcconfig
 xcodebuild -project People.ai.xcodeproj -scheme "People.ai" -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" archive -archivePath "Build/People.ai.xcarchive"
-xcodebuild -project People.ai.xcodeproj -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" -exportArchive -archivePath "Build/People.ai.xcarchive" -exportOptionsPlist "exportOptions.plist"
+#xcodebuild -project People.ai.xcodeproj -configuration Release "CODE_SIGN_IDENTITY=Developer ID Application: People.ai, Inc (B865KYJU5B)" -UseModernBuildSystem=NO -exportArchive -archivePath "Build/People.ai.xcarchive"  -exportOptionsPlist "exportOptions.plist"
 
 # package
 packagesbuild -v "People.ai.pkgproj"
